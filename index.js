9e72dc9f247348640abf3c7cc7bcb4a8aaa31bf9
skyorion427 <skyorion427@gmail.com>
2018-11-05 08:00:14 +0100

    added configuration

--- index.js
+++ index.js
@@ -5,95 +5,16 @@ import { setOptions } from '@storybook/addon-options';
 import { withBackgrounds } from '@storybook/addon-backgrounds';
 import { withNotes } from '@storybook/addon-notes';
 import { withKnobs } from '@storybook/addon-knobs';
+const Twig = require('twig');
+const showdown = require('showdown');
 import {
   asset,
   path,
   csrf_token,
 } from './twigFunctions';
 
-export const configuration = () => {
-
-  setOptions({
-    /**
-     * name to display in the top left corner
-     * @type {String}
-     */
-    name: 'Plaza UI',
-    /**
-     * URL for name in top left corner to link to
-     * @type {String}
-     */
-    url: '#',
-    /**
-     * show story component as full screen
-     * @type {Boolean}
-     */
-    goFullScreen: false,
-    /**
-     * display panel that shows a list of stories
-     * @type {Boolean}
-     */
-    showStoriesPanel: true,
-    /**
-     * display panel that shows addon configurations
-     * @type {Boolean}
-     */
-    showAddonPanel: true,
-    /**
-     * display floating search box to search through stories
-     * @type {Boolean}
-     */
-    showSearchBox: false,
-    /**
-     * show addon panel as a vertical panel on the right
-     * @type {Boolean}
-     */
-    addonPanelInRight: false,
-    /**
-     * sorts stories
-     * @type {Boolean}
-     */
-    sortStoriesByKind: false,
-    /**
-     * regex for finding the hierarchy separator
-     * @example:
-     *   null - turn off hierarchy
-     *   /\// - split by `/`
-     *   /\./ - split by `.`
-     *   /\/|\./ - split by `/` or `.`
-     * @type {Regex}
-     */
-    hierarchySeparator: null,
-    /**
-     * regex for finding the hierarchy root separator
-     * @example:
-     *   null - turn off multiple hierarchy roots
-     *   /\|/ - split by `|`
-     * @type {Regex}
-     */
-    hierarchyRootSeparator: null,
-    /**
-     * sidebar tree animations
-     * @type {Boolean}
-     */
-    sidebarAnimations: true,
-    /**
-     * id to select an addon panel
-     * @type {String}
-     */
-    selectedAddonPanel: undefined, // The order of addons in the "Addon panel" is the same as you import them in 'addons.js'. The first panel will be opened by default as you run Storybook
-    /**
-     * enable/disable shortcuts
-     * @type {Boolean}
-     */
-    enableShortcuts: true, // true by default
-  });
-
-  const Twig = require('twig');
-
-  var showdown = require('showdown');
-
-  var converter = new showdown.Converter();
+const configuration = () => {
+  const converter = new showdown.Converter();
 
   Twig.extendFilter('markdown', function (value) {
       return converter.makeHtml(value);
@@ -111,7 +32,6 @@ export const configuration = () => {
     return csrf_token(value);
   });
 
-
   addDecorator(
     withBackgrounds([
       { name: 'white', value: '#fff', default: true },
@@ -123,87 +43,103 @@ export const configuration = () => {
   
   addDecorator(withNotes);
   addDecorator(withKnobs);
+}
 
+export const setOptions = (options) => {
+  const {
+    name = '',
+    url = '#',
+    goFullScreen = false,
+    showStoriesPanel = true,
+    showAddonPanel = true,
+    showSearchBox = false,
+    addonPanelInRight = false,
+    sortStoriesByKind = false,
+    hierarchySeparator = null,
+    hierarchyRootSeparator = null,
+    sidebarAnimations = true,
+    selectedAddonPanel = undefined,
+    enableShortcuts = true,
+  } = options;
+  
   setOptions({
-  /**
-   * name to display in the top left corner
-   * @type {String}
-   */
-  name: 'Plaza UI',
-  /**
-   * URL for name in top left corner to link to
-   * @type {String}
-   */
-  url: '#',
-  /**
-   * show story component as full screen
-   * @type {Boolean}
-   */
-  goFullScreen: false,
-  /**
-   * display panel that shows a list of stories
-   * @type {Boolean}
-   */
-  showStoriesPanel: true,
-  /**
-   * display panel that shows addon configurations
-   * @type {Boolean}
-   */
-  showAddonPanel: true,
-  /**
-   * display floating search box to search through stories
-   * @type {Boolean}
-   */
-  showSearchBox: false,
-  /**
-   * show addon panel as a vertical panel on the right
-   * @type {Boolean}
-   */
-  addonPanelInRight: false,
-  /**
-   * sorts stories
-   * @type {Boolean}
-   */
-  sortStoriesByKind: false,
-  /**
-   * regex for finding the hierarchy separator
-   * @example:
-   *   null - turn off hierarchy
-   *   /\// - split by `/`
-   *   /\./ - split by `.`
-   *   /\/|\./ - split by `/` or `.`
-   * @type {Regex}
-   */
-  hierarchySeparator: null,
-  /**
-   * regex for finding the hierarchy root separator
-   * @example:
-   *   null - turn off multiple hierarchy roots
-   *   /\|/ - split by `|`
-   * @type {Regex}
-   */
-  hierarchyRootSeparator: null,
-  /**
-   * sidebar tree animations
-   * @type {Boolean}
-   */
-  sidebarAnimations: true,
-  /**
-   * id to select an addon panel
-   * @type {String}
-   */
-  selectedAddonPanel: undefined, // The order of addons in the "Addon panel" is the same as you import them in 'addons.js'. The first panel will be opened by default as you run Storybook
-  /**
-   * enable/disable shortcuts
-   * @type {Boolean}
-   */
-  enableShortcuts: true, // true by default
-});
-
+    /**
+     * name to display in the top left corner
+     * @type {String}
+     */
+    name,
+    /**
+     * URL for name in top left corner to link to
+     * @type {String}
+     */
+    url,
+    /**
+     * show story component as full screen
+     * @type {Boolean}
+     */
+    goFullScreen,
+    /**
+     * display panel that shows a list of stories
+     * @type {Boolean}
+     */
+    showStoriesPanel,
+    /**
+     * display panel that shows addon configurations
+     * @type {Boolean}
+     */
+    showAddonPanel,
+    /**
+     * display floating search box to search through stories
+     * @type {Boolean}
+     */
+    showSearchBox,
+    /**
+     * show addon panel as a vertical panel on the right
+     * @type {Boolean}
+     */
+    addonPanelInRight,
+    /**
+     * sorts stories
+     * @type {Boolean}
+     */
+    sortStoriesByKind,
+    /**
+     * regex for finding the hierarchy separator
+     * @example:
+     *   null - turn off hierarchy
+     *   /\// - split by `/`
+     *   /\./ - split by `.`
+     *   /\/|\./ - split by `/` or `.`
+     * @type {Regex}
+     */
+    hierarchySeparator,
+    /**
+     * regex for finding the hierarchy root separator
+     * @example:
+     *   null - turn off multiple hierarchy roots
+     *   /\|/ - split by `|`
+     * @type {Regex}
+     */
+    hierarchyRootSeparator,
+    /**
+     * sidebar tree animations
+     * @type {Boolean}
+     */
+    sidebarAnimations,
+    /**
+     * id to select an addon panel
+     * @type {String}
+     */
+    selectedAddonPanel, // The order of addons in the "Addon panel" is the same as you import them in 'addons.js'. The first panel will be opened by default as you run Storybook
+    /**
+     * enable/disable shortcuts
+     * @type {Boolean}
+     */
+    enableShortcuts, // true by default
+  });
 }
 
 export const AddStories = (templateFiles, templateData) => {
-  
   configuration();
 
   templateFiles.keys().forEach(pathName => {
